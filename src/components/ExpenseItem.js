97529be884dad359da6d56bfee712cc5cce836af
Kathy Tavia <kathy.tavia@gmail.com>
2021-08-07 17:43:03 +0200

    updated css for expense item details

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import "./ExpenseItem.css";
 const ExpenseItem = () => {
     return (
         <div className="expense-item">
-            <div>Aug 8, 2021</div>
+            <div className="expense-item-date">Aug 8, 2021</div>
             <div className="expense-item-description">
                 <h2>Rent</h2>
                 <div className="expense-item-price">$250</div>
