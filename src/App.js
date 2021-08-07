b8285b11d22b2191465a66a5e1e9de8412dc0b69
Kathy Tavia <kathy.tavia@gmail.com>
2021-08-07 10:25:02 +0200

    added expense item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
-  
+const App = () => {
+  return (
+      <div>
+          <h2>Let's get started</h2>
+          <ExpenseItem></ExpenseItem>
+      </div>
+  )
 }
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = () => {
+    return (
+        <div>
+            <div>August 7, 2021</div>
+            <div>
+                <h2>Rent</h2>
+                <h2>250 euros</h2>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
