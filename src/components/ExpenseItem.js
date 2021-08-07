12ef56214d41df8a37dddaca85146db25a7ed79b
Kathy Tavia <kathy.tavia@gmail.com>
2021-08-07 18:56:00 +0200

    changed format for item dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     return (
         <div className="expense-item">
-            <div className="expense-item-date">{props.date.toISOString()}</div>
+            <div className="expense-item-date">   
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item-description">
-                <h2>{props.title}}</h2>
-                <div className="expense-item-price">{props.amount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item-price">$ {props.amount}</div>
             </div>
         </div>
     );
