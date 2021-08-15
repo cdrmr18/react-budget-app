13cf3d676b37f65ddfb1750da9e4e590dde287ac
Kathy Tavia <kathy.tavia@gmail.com>
2021-08-15 12:39:53 +0200

    added event listners to form with functions to collect input values

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -1,20 +1,37 @@
 import "./NewExpenseForm.css";
+import React, { useState} from "react";
 
 const NewExpenseForm = () => {
+    const [title, setEnteredTitle] = useState('');
+    const [amount, setEnteredAmount] = useState('');
+    const [date, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (e) => {
+        setEnteredTitle(e.target.value)
+    }
+    
+    const amountChangeHandler = (e) => {
+        setEnteredAmount(e.target.value)
+    }
+
+    const dateChangeHandler = (e) => {
+        setEnteredDate(e.target.value)
+    }
+    
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31"/>
+                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
                 </div>
                 <div className="new-expense__actions">
                     <button type="submit">Add Expenses</button>
@@ -22,6 +39,6 @@ const NewExpenseForm = () => {
             </div>
         </form>
     )
-}
+};
 
 export default NewExpenseForm;
